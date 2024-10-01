ec3471b3cf7fd79dc8e0f3a32b0bd7db2b45571b
ioan.tugui <ioan.tugui@gmail.com>
2024-10-01 10:35:23 +0300

    Add about me

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,27 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
-// const inter = Inter({
-//   subsets: ["latin"],
-//   variable: "--font-inter",
-//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-// });
-
 export const metadata: Metadata = {
-  title: "Engineering Hub",
-  description: "Software engineering blog and portfolio",
+  title: "Engineering Hub - .NET, React, and Cloud Development Blog",
+  description: "Follow my journey as a .NET developer tackling React and Cloud challenges. Discover tips, tricks, and real-world solutions for modern software engineering and web development.",
+  keywords: ['.NET', 'React', 'AWS', 'developer blog', 'cloud computing', 'web development', 'coding challenges', 'serverless', 'software engineering'],
+  openGraph: {
+    title: 'Engineering Hub - .NET, React, and AWS Development Blog',
+    description: 'Follow my journey as a .NET developer tackling React and AWS challenges. Discover tips, tricks, and real-world solutions for modern software engineering and web development.',
+    url: 'https://www.itugui.com',
+    type: 'website',
+    siteName: 'ITugui - Engineering Hub Blog',
+    locale: 'en_US',
+  },
+
+  twitter: {
+    card: 'summary',
+    title: 'Engineering Hub - .NET, React, and AWS Development Blog',
+    description: 'Follow my journey as a .NET developer tackling React and AWS challenges. Discover tips, tricks, and real-world solutions for modern software engineering and web development.',
+    site: '@itugui',
+  },
   icons: {
     icon: [
       { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/images/favicon-32x32.png' },
@@ -20,7 +29,15 @@ export const metadata: Metadata = {
     ],
     apple: '/images/apple-touch-icon.png',
   },
-  applicationName: 'MyApp',
+  authors: [{ name: '@itugui', url: 'https://www.itugui.com' }],
+  applicationName: 'ITugui - Engineering Hub Blog',
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: 'https://www.itugui.com',
+  },
 };
 export const viewport = {
   themeColor: [
@@ -35,9 +52,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      // className={`${inter.variable} antialiased`}
-      >
+      <body>
         <Header />
         {children}
         <Footer />
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 export default async function Home() {
 
   const posts = getRecentPosts();
-  
+
   return (
     <main className="mx-auto flex w-full max-w-7xl flex-col items-start justify-start px-4 pt-16 md:flex-row">
       <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-800">
@@ -24,7 +24,7 @@ export default async function Home() {
 const HeroSection = () => (
   <section className="py-20 text-center">
     <div className="container mx-auto px-4">
-      <h1 className="text-5xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+      <h1 className="text-5xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 leading-none">
         Welcome to my hub
       </h1>
       <p className="text-xl text-gray-600 dark:text-gray-300 mb-8 max-w-2xl mx-auto">
@@ -49,7 +49,7 @@ const RecentPosts = ({ posts }: { posts: PostPreview[] }) => (
           <Card key={post.slug} className="flex flex-col overflow-hidden transition-all duration-300 hover:shadow-xl hover:-translate-y-1">
             <div className="relative">
               <img
-                src={post.image}
+                src={'/placeholder.svg?height=150&width=300'}
                 alt={post.title}
                 className="object-cover w-full h-[150px]"
               />
