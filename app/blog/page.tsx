7335893fec342375985a7a56ed9a3b26d67f6d12
ioan.tugui <ioan.tugui@gmail.com>
2025-12-15 13:04:48 +0200

    add h1 for blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -26,7 +26,12 @@ export default function BlogHomePage() {
 
     return (
         <main className="mx-auto flex w-full max-w-7xl flex-col items-start justify-start px-4 pt-16 md:flex-row">
-            <Posts />
+            <div className="min-h-screen">
+                <section className='w-full py-12'>
+                    <h1 className='mb-2 text-3xl font-bold'>My Blog</h1>
+                    <p className="text-lg text-muted-foreground">Here you'll find my thoughts, tutorials, and experiences in the world of software development, focusing on .NET, React, and AWS.</p>                </section>
+                <Posts />
+            </div>
         </main>
     )
 }
